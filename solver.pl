6a8d3fad4b28e54e19fc9c26d5264f699d91e756
Rod Doe <doerodney@gmail.com>
2026-04-09 17:08:19 -0700

    Generally working, some cleanup remains.

--- solver.pl
+++ solver.pl
@@ -3,7 +3,7 @@ use strict;
 my $WORD_LIST_PATH = '/usr/share/dict/words'; 
 my @CLUE_WORD_LIST = ();   # The list of clue words
 my @REGEX_LIST = ();       # The list of clue words converted to regex for search
-my @GUESS_INDEX = ();      # The index of the missing letter in each word
+
 
 sub find_clue_word_matches {
   my ($regex, $ra) = @_;
@@ -17,10 +17,11 @@ sub find_clue_word_matches {
 
 
 sub generate_search_regex {
-  # This is the regex comprised of all the clue word matches that is used to query the result:
+  # This returns the regex comprised of all the clue word matches that is used to query the result:
   my $ra = shift;
   my $counter = 0;
-  my $regex = '^';
+  my $regex = '';
+  my $result = '^';
 
   # This section creates a regex for the entire keyword.
   # For these clues: sn_p, _erby, _eel, pe_, ste_n, g_ntly
@@ -28,44 +29,27 @@ sub generate_search_regex {
   # ^[ai][d][fhkpr][aegnprtw][ir][e]$
   #
   foreach $regex (@REGEX_LIST) {
-    my $clue_word = $CLUE_WORD_LIST[$counter];
-    my $idx = index($clue_word, '_');
-    my @matches = find_clue_word_matches($regex, $ra);
-    chomp(@matches);  # ?necessary
-    my $match = '';
-    my $match_text = join(',', @matches);
-    print("Matches for $regex: $match_text\n"); 
+    my $clue = $CLUE_WORD_LIST[$counter];
+    my $idx = index($clue, '_');
+    my @words = grep { /$regex/ } @$ra;
+    my $word = '';
+    my $text = join(',', @words);
+    print("Matches for $regex: $text\n"); 
 
-#    my $character_class = '[';
-#    foreach $match (@matches) {
-#      my $character = substr($match, $idx, 1);
-#      $character_class .= $character;
-#    }
-#    $character_class .= ']';
-#    $regex .= $character_class;
+    my @chars;
+    my $class = '[';
+    foreach $word (@words) {
+      my $char = substr($word, $idx, 1);
+      $class .= $char;
+    }
+    $class .= ']';
+    $result .= $class;
     $counter++;
   }
 
-  $regex .= '$';
+  $result .= '$';
 
-  return $regex;
-}
-
-
-sub load_guess_index_list() {
-  my $clueWord = '';
-  my $idx = 0;
-
-  foreach $clueWord (@CLUE_WORD_LIST) {
-    $idx = index($clueWord, '_');
-    if ($idx > -1 ) {
-      print("The guess index for $clueWord is $idx\n");
-      push(@GUESS_INDEX, $idx);
-    } else {
-      print("$clueWord has no underscore\n");
-      exit(1);
-    }
-  }
+  return $result;
 }
 
 
@@ -80,7 +64,6 @@ sub load_regex_list() {
     $regex = '^' . $clueWord;
     $regex .= '$';
     $regex =~ s/_/$substr/;
-    print("Clue word $clueWord becomes regex $regex\n");
     push(@REGEX_LIST, $regex);
   }
 }
@@ -103,7 +86,9 @@ my $nArgs = scalar(@ARGV);
 if ($nArgs != 6) {
   @CLUE_WORD_LIST = @TEST_CLUE_WORD_LIST;
 } else {
-  @CLUE_WORD_LIST = @ARGV;
+  for (my $i = 0; $i < 6; $i++) {
+    push(@CLUE_WORD_LIST,  lc($ARGV[$i]));
+  }
 }
 
 my $nClueWords = scalar(@CLUE_WORD_LIST);
@@ -112,14 +97,8 @@ my $clueWord = '';
 if ($nClueWords != 6) {
   usage();
   exit(1);
-} else {
-  foreach $clueWord (@CLUE_WORD_LIST) {
-    print "$clueWord\n"
-  }
-  print("\n");
-}
+} 
 
-load_guess_index_list();
 load_regex_list();
 
 open(SRC, "<$WORD_LIST_PATH") or die "Cannot open $WORD_LIST_PATH (read-only)" ;
@@ -129,6 +108,11 @@ chomp(@lines);
 
 my $regex = generate_search_regex(\@lines);
 print("search regex: $regex\n");
+my @solutions = grep { /$regex/ } @lines;
+my $solution;
+foreach $solution (@solutions) {
+  print("Solution: $solution\n");
+}
 
 # my @test_list = qw(Amanda ban can fan man mancave pan plan ran tan than van wan); 
 #find_clue_word_matches '^[a-z]an$', \@lines;   # ban can fan man pan ran tan van wan 
